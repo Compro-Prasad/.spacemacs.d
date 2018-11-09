9cd88ee477c69d977fd0ba38fff4c5abd5663a30
Compro-Prasad <comproprasad@gmail.com>
2018-11-09 10:37:16 +0530

    Reduce font size and separator scale

--- init.el
+++ init.el
@@ -272,7 +272,7 @@ It should only modify the values of Spacemacs settings."
    ;; refer to the DOCUMENTATION.org for more info on how to create your own
    ;; spaceline theme. Value can be a symbol or list with additional properties.
    ;; (default '(spacemacs :separator wave :separator-scale 1.5))
-   dotspacemacs-mode-line-theme '(spacemacs :separator slant :separator-scale 1.5)
+   dotspacemacs-mode-line-theme '(spacemacs :separator slant :separator-scale 1.4)
 
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
@@ -281,7 +281,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Ubuntu Mono"  ; "Source Code Pro"
-                               :size 16
+                               :size 15
                                :weight normal
                                :width normal)
 
