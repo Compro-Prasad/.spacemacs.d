8acc251628b0bcd4e52bc80344fe2446265b7a54
Compro-Prasad <comproprasad@gmail.com>
2019-05-14 11:06:20 +0530

    No need to configure ccls and org latex user labels, they are merged

--- compro-emacs.el
+++ compro-emacs.el
@@ -10,7 +10,6 @@
 
 (setq
  spaceline-org-clock-p t  ; To permanently enable mode line display of org clock
- org-latex-prefer-user-labels t  ; Support for CUSTOM_ID in org export latex:
  org-startup-indented t  ; Always turn on org-indent-mode
  )
 ;;   Org mode end
@@ -29,48 +28,6 @@
             (when (derived-mode-p 'prog-mode)
               (flyspell-mode-off))))
 ;;   Spell check end
-
-
-;;;;;;;;;;;;;;;;;;;;;;;
-;;                   ;;
-;;   Miscellaneous   ;;
-;;                   ;;
-;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
-
-;;;;;;;;;;;;;
-;;         ;;
-;;   C++   ;;
-;;         ;;
-;;;;;;;;;;;;;
-
-;; (use-package ccls
-;;   :commands lsp-ccls-enable
-;;   :init
-;;   (defun ccls/enable ()
-;;     "Enable ccls in current buffer."
-;;     (interactive)
-;;     (condition-case nil
-;;         (lsp-ccls-enable)
-;;       (user-error nil)))
-
-;;   (defun ccls/enable-hook ()
-;;     "Enable ccls whenever a C/C++ file opens."
-;;     (interactive)
-;;     (add-hook 'c-mode-common-hook #'ccls/enable))
-
-;;   (defun ccls/disable-hook ()
-;;     "Remove ccls from c-mode-common-hook."
-;;     (interactive)
-;;     (remove-hook 'c-mode-common-hook #'ccls/enable))
-;;   (with-eval-after-load 'c++-mode
-;;     (define-key c++-mode-map (kbd "M-.") 'lsp-ui-peek-jump-forward)
-;;     (define-key c++-mode-map (kbd "M-,") 'lsp-ui-peek-jump-backward)))
-;;   C++ end
-
 
 
 ;;;;;;;;;;;;;;;
