2410958ee5c88bedf97c4bd9c3074de4c2b2bebc
Compro-Prasad <comproprasad@gmail.com>
2019-06-14 07:26:09 +0530

    Revert "Added telega as an additional package"
    This reverts commit e56e47a62c42a18232ca83344789cda37f6c55be.

--- init.el
+++ init.el
@@ -146,7 +146,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(ov undo-propose ccls monokai-theme molokai-theme org-gcal telega)
+   dotspacemacs-additional-packages '(ov undo-propose ccls monokai-theme molokai-theme org-gcal)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
